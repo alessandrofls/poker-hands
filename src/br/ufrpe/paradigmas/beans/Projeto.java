3741e64721ba3fed0d8cb578ea55e210b5b90e97
Alessandro <alessandrolimabcc@gmail.com>
2018-07-10 11:28:15 -0300

    Alterações simples
    apenas para manter o sistema up to date

--- src/br/ufrpe/paradigmas/beans/Projeto.java
+++ src/br/ufrpe/paradigmas/beans/Projeto.java
@@ -9,22 +9,23 @@ import java.util.Arrays;
 public class Projeto{
 	
 	public static void main(String args[]) {
-		String maosPoker [][];
+		String maosPoker[];
 		try {
 			
 			final String CAMINHO_ARQUIVO = "C:\\Users\\Alessandro\\Desktop\\poker2K.txt";
 			FileReader arq = new FileReader(CAMINHO_ARQUIVO);
 		    BufferedReader lerArq = new BufferedReader(arq);
 		    
-		    String linha = lerArq.readLine();
-		    linha = linha.replaceAll("\\s", "");
-		    
+		    String linha = lerArq.readLine();		    
 		    
 		    while (linha != null) {
+		    	linha = linha.replaceAll("\\s", "");
+		    	
+		    	
 		        System.out.println(linha);
 		 
 		        linha = lerArq.readLine(); // lê da segunda até a última linha
-		        linha = linha.replaceAll("\\s", "");
+		        
 		      }
 		 
 		      arq.close();

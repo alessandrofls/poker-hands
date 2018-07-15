4323cf4b8a0cfc0f55f431bfcffb3331a016f1ea
Alessandro <alessandrolimabcc@gmail.com>
2018-07-15 14:15:57 -0300

    Alterações incompetentes

--- src/br/ufrpe/paradigmas/beans/Projeto.java
+++ src/br/ufrpe/paradigmas/beans/Projeto.java
@@ -3,6 +3,8 @@ package br.ufrpe.paradigmas.beans;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Projeto{
@@ -10,49 +12,81 @@ public class Projeto{
 	public static void main(String args[]) {
 		try {
 
-			final String CAMINHO_ARQUIVO = "C:\\Users\\Alessandro\\Desktop\\poker2K.txt";
-			FileReader arq = new FileReader(CAMINHO_ARQUIVO);
+			FileReader arq = new FileReader("C:\\Users\\Alessandro\\Desktop\\poker2M.txt");
 			BufferedReader lerArq = new BufferedReader(arq);
 
-			String linha = lerArq.readLine();		    
+			String linha;
+			int totalQuatroIguais= 0;
+			int totalDiferente = 0;
+			int totalSequencia = 0;
 
-			int quatroIguais = 0;
-			int diferentes = 0;
-
-			while (linha != null) {
+			while ((linha = lerArq.readLine()) != null) {
 				linha = linha.replaceAll("\\s", "");
-				int igual = 0;
-				int diferente = 0;
-				for(int i = 0; i < linha.length(); i++) {
-					int j = 0;
-					//percorre a linha verificando o caracter no indice i com o restante da linha;
-					do {  			
-						if(linha.charAt(i) == linha.charAt(j)) {
-							igual++;
-						}else if(linha.charAt(i) != linha.charAt(j)) {
-							diferente++;
-						}
-						j++;
-					}while(j < linha.length());
-					
-					if(igual == 4) {
-						quatroIguais++;
-					}else if(diferente == 5) {
-						diferentes++;
-				}
 
-				
-				}
+				totalQuatroIguais += iguais(linha);
+				totalSequencia += sequencia(linha);
+				totalDiferente += sequencia(linha);
 
 				linha = lerArq.readLine(); // lê da segunda até a última linha
-
 			}
-			System.out.println(quatroIguais +"  "+ diferentes);
+			System.out.println(totalQuatroIguais + " " + totalSequencia + " " + totalDiferente );
 
 			arq.close();
 		}catch(IOException e) {
 			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
 		}
+
+	}
+	public static int iguais(String teste) {
+		int count = 0;
+		int result = 0;
+		for(int i = 0; i < teste.length(); i++) {
+			for(int j = i+1; j < teste.length(); j++) {
+				if(teste.charAt(i) == teste.charAt(j)) {
+					count += 1;
+				}
+			}
+		}
+		if (count == 4) {
+			result = 1;
+		}
+
+		return result;
 	}
 
-}
+
+	public static int diferentes(String teste) {
+		int count = 0;
+		int result = 0;
+		for(int i = 0; i < teste.length(); i++) {
+			for(int j = i+1; j < teste.length(); j++) {
+				if(teste.charAt(i) != teste.charAt(j)) {
+					count += 1;
+				}
+			}
+		}
+		if (count == 4) {
+			result = 1;
+		}
+
+		return result;
+	}
+
+	public static int sequencia(String teste) {
+		int count = 0;
+		int result = 0;
+		for(int i = 0; i < teste.length(); i++) {
+			for(int j = i+1; j < teste.length(); j++) {
+				if(teste.charAt(i) < teste.charAt(j)) {
+					count += 1;
+				}
+			}
+		}
+			if(count == 5) {
+				result = 1;
+			}
+
+			return result;
+		}
+	}
+

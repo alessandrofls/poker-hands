496acb6c74aec754055d441f65146ec199762335
Alessandro <alessandrolimabcc@gmail.com>
2018-07-16 08:10:44 -0300

    Update de metodos e forma de saída
    criação de métodos de criação de arquivo txt e escrita no mesmo

--- src/br/ufrpe/paradigmas/beans/Projeto.java
+++ src/br/ufrpe/paradigmas/beans/Projeto.java
@@ -1,31 +1,38 @@
 package br.ufrpe.paradigmas.beans;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.PrintWriter;
 
 
 public class Projeto{
 
 	public static void main(String args[]) {
-		long tempoInicio = System.currentTimeMillis();
+		principal("C:\\Users\\Alessandro\\eclipse-workspace\\poker-hands\\bin\\br\\ufrpe\\paradigmas\\beans\\poker2K.txt");
+		principal("C:\\Users\\Alessandro\\Desktop\\poker2M.txt");
+		principal("C:\\Users\\Alessandro\\Desktop\\poker200M.txt");
+
 		
+	}
+
+	public static void principal(String path) {
+		long tempoInicio = System.currentTimeMillis();
 		String linha;
 		int totalQuatroIguais= 0;
 		int totalDiferente = 0;
 		int totalSequencia = 0;
-		
+
 		try {
 
-			FileReader arq = new FileReader("C:\\Users\\Alessandro\\eclipse-workspace\\poker-hands\\src\\br\\ufrpe\\paradigmas\\beans\\poker2k.txt");
+			FileReader arq = new FileReader(path);
 			BufferedReader lerArq = new BufferedReader(arq);
 
 			while ((linha = lerArq.readLine()) != null) {
 				linha = linha.replaceAll("\\s", "");
-				
-				System.out.println(linha.length());
 
 				totalQuatroIguais += iguais(linha);
 				totalSequencia += sequencia(linha);
@@ -33,16 +40,17 @@ public class Projeto{
 
 				linha = lerArq.readLine(); // lê da segunda até a última linha
 			}
-			System.out.println(totalQuatroIguais + " " + totalSequencia + " " + totalDiferente );
-
 			arq.close();
 		}catch(IOException e) {
 			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
 		}
+
+		long tempo = System.currentTimeMillis()-tempoInicio;
 		
-		System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
+		escreverNoArquivo(tempo, totalQuatroIguais, totalDiferente, totalSequencia);
 
 	}
+
 	public static int iguais(String teste) {
 		int count = 0;
 		int result = 0;
@@ -60,13 +68,12 @@ public class Projeto{
 		return result;
 	}
 
-
 	public static int diferentes(String teste) {
 		int count = 0;
 		int result = 0;
 		for(int i = 0; i < teste.length(); i++) {
 			for(int j = i+1; j < teste.length(); j++) {
-				if(teste.codePointAt(i) != teste.codePointAt(j) && !((teste.codePointAt(i) < teste.codePointAt(j)))) {
+				if((teste.codePointAt(i) != teste.codePointAt(j)) && !((teste.codePointAt(i) < teste.codePointAt(j)))) {
 					count += 1;
 				}
 			}
@@ -88,11 +95,38 @@ public class Projeto{
 				}
 			}
 		}
-			if(count == 4) {
-				result = 1;
+		if(count == 4) {
+			result = 1;
+		}
+
+		return result;
+	}
+
+	public static void escreverNoArquivo(long tempo, int quatroIguais, int todasDiferentes, int sequencias) {
+		try {
+
+			// Conteudo
+			String content = tempo + " " + quatroIguais + " " + todasDiferentes + " " + sequencias;
+
+			// Cria arquivo
+			File file = new File("teste.txt");
+
+			// Se o arquivo nao existir, ele gera
+			if (!file.exists()) {
+				file.createNewFile();
 			}
 
-			return result;
+			// Prepara para escrever no arquivo
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter saida = new PrintWriter(bw);
+
+			// Escreve e fecha arquivo
+			saida.println(content);
+			saida.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+}
 
